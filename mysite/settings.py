650d97c41fbe47d7023b7d26695264aef50c4c20
Aylin Kazi <aylin.kazi@gmail.com>
2020-09-08 15:47:35 +0200

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#0sk$4!j4dw4956v13yphm%2h8pq=9%5+5-9kd1ol&fmn&_6*a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "kazlyn.pythonanywhere.com"]
 
 
 # Application definition
